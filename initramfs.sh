0247aabfc5a8d21351012a95e85c16feda36e4a1
Maxime Daniel <root@maxux.net>
2016-11-23 13:23:14 +0100

    improve the initramfs build script

--- initramfs.sh
+++ initramfs.sh
@@ -15,6 +15,14 @@ FUSE_CHECKSUM="91c97e5ae0a40312115dfecc4887bd9d"
 CERTS_VERSION="20161102"
 CERTS_CHECKSUM="5f26fc332ef3c588814c21bf4766ffa1"
 
+PARTED_VERSION="3.2"
+PARTED_CHECKSUM="0247b6a7b314f8edeb618159fa95f9cb"
+
+LINUXUTILS_VERSION="2.29"
+LINUXUTILS_CHECKSUM="07b6845f48a421ad5844aa9d58edb837"
+
+
+
 # You need to use absolutes path
 DISTFILES="${PWD}/archives"
 WORKDIR="${PWD}/staging"
@@ -27,11 +35,13 @@ KERNEL_LINK="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_VERSION
 BUSYBOX_LINK="https://www.busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2"
 FUSE_LINK="https://github.com/libfuse/libfuse/archive/fuse-${FUSE_VERSION}.tar.gz"
 CERTS_LINK="http://ftp.fr.debian.org/debian/pool/main/c/ca-certificates/ca-certificates_${CERTS_VERSION}_all.deb"
+PARTED_LINK="http://ftp.gnu.org/gnu/parted/parted-${PARTED_VERSION}.tar.xz"
+LINUXUTILS_LINK="https://www.kernel.org/pub/linux/utils/util-linux/v2.29/util-linux-${LINUXUTILS_VERSION}.tar.xz"
 
 #
 # Flags
 #
-OPTS=$(getopt -o dbfsckh --long download,busybox,fuse,ssl,cores,kernel,help -n 'parse-options' -- "$@")
+OPTS=$(getopt -o dbtckh --long download,busybox,tools,cores,kernel,help -n 'parse-options' -- "$@")
 if [ $? != 0 ]; then
     echo "Failed parsing options." >&2
     exit 1
@@ -44,8 +54,7 @@ if [ "$OPTS" != " --" ]; then
 
     DO_DOWNLOAD=0
     DO_BUSYBOX=0
-    DO_FUSE=0
-    DO_CERTS=0
+    DO_TOOLS=0
     DO_CORES=0
     DO_KERNEL=0
 
@@ -56,16 +65,14 @@ while true; do
     case "$1" in
         -d | --download) DO_DOWNLOAD=1; shift ;;
         -b | --busybox)  DO_BUSYBOX=1;  shift ;;
-        -f | --fuse)     DO_FUSE=1;     shift ;;
-        -s | --ssl)      DO_CERTS=1;    shift ;;
+        -t | --tools)    DO_TOOLS=1;    shift ;;
         -c | --cores)    DO_CORES=1;    shift ;;
         -k | --kernel)   DO_KERNEL=1;   shift ;;
         -h | --help)
             echo "Usage:"
             echo " -d --download    only download and extract archives"
             echo " -b --busybox     only (re)build busybox"
-            echo " -f --fuse        only (re)build libfuse"
-            echo " -s --ssl         only (re)build ca-certificates"
+            echo " -t --tools       only (re)build tools (ssl, fuse, ...)"
             echo " -c --cores       only (re)build core0 and coreX"
             echo " -k --kernel      only (re)build kernel (produce final image)"
             echo " -h --help        display this help message"
@@ -153,6 +160,8 @@ download() {
     download_file $BUSYBOX_LINK $BUSYBOX_CHECKSUM
     download_file $FUSE_LINK $FUSE_CHECKSUM
     download_file $CERTS_LINK $CERTS_CHECKSUM
+    download_file $PARTED_LINK $PARTED_CHECKSUM
+    download_file $LINUXUTILS_LINK $LINUXUTILS_CHECKSUM
 
     popd
 }
@@ -188,6 +197,16 @@ extract() {
         popd
 
         rm -rf "ca-certificates-${CERTS_VERSION}/temp"
+    fi
+
+    if [ ! -d "parted-${PARTED_VERSION}" ]; then
+        echo "[+] extracting: parted-${PARTED_VERSION}"
+        tar -xf ${DISTFILES}/parted-${PARTED_VERSION}.tar.xz -C .
+    fi
+
+    if [ ! -d "util-linux-${LINUXUTILS_VERSION}" ]; then
+        echo "[+] extracting: util-linux-${LINUXUTILS_VERSION}"
+        tar -xf ${DISTFILES}/util-linux-${LINUXUTILS_VERSION}.tar.xz -C .
     fi
 
     popd
@@ -228,7 +247,7 @@ build_busybox() {
 prepare_fuse() {
     echo "[+] preparing fuse"
     ./makeconf.sh
-    ./configure --prefix /
+    ./configure --prefix /usr
 }
 
 compile_fuse() {
@@ -297,6 +316,7 @@ compile_kernel() {
 }
 
 install_kernel() {
+    ls -alh arch/x86/boot/bzImage
     return
 }
 
@@ -304,8 +324,8 @@ build_kernel() {
     pushd "${WORKDIR}/linux-${KERNEL_VERSION}"
 
     prepare_kernel
-    # compile_kernel
-    # install_kernel
+    compile_kernel
+    install_kernel
 
     popd
 }
@@ -343,12 +363,107 @@ build_cores() {
     popd
 }
 
+# parted
+prepare_parted() {
+    echo "[+] configuring parted"
+    ./configure --prefix "${ROOTDIR}"/usr --disable-device-mapper
+
+    if [ ! -f parted-3.2-devmapper.patch ]; then
+        echo "[+] downloading patch"
+        curl -s https://gist.githubusercontent.com/maxux/a5472530dd88b3480d745388d81e4c7f/raw/d5b67d7bd7714178b3ebe35a2836f64ccaa32431/parted-3.2-devmapper.patch > parted-3.2-devmapper.patch
+        patch -p1 < parted-3.2-devmapper.patch
+    fi
+}
+
+compile_parted() {
+    make ${MAKEOPTS}
+}
+
+install_parted() {
+    make install
+}
+
+build_parted() {
+    pushd "${WORKDIR}/parted-${PARTED_VERSION}"
+
+    prepare_parted
+    compile_parted
+    install_parted
+
+    popd
+}
+
+# util-linux
+prepare_linuxutil() {
+    echo "[+] configuring util-linux"
+    ./configure --prefix "${ROOTDIR}"/usr \
+        --disable-libfdisk \
+        --disable-mount \
+        --disable-zramctl \
+        --disable-mountpoint \
+        --disable-eject \
+        --disable-lslogins \
+        --disable-setpriv \
+        --disable-agetty \
+        --disable-cramfs \
+        --disable-bfs \
+        --disable-minix \
+        --disable-fdformat \
+        --disable-wdctl \
+        --disable-cal \
+        --disable-logger \
+        --disable-switch_root \
+        --disable-pivot_root \
+        --disable-ipcrm \
+        --disable-ipcs \
+        --disable-kill \
+        --disable-last \
+        --disable-utmpdump \
+        --disable-mesg \
+        --disable-raw \
+        --disable-rename \
+        --disable-login \
+        --disable-nologin \
+        --disable-sulogin \
+        --disable-su \
+        --disable-runuser \
+        --disable-ul \
+        --disable-more \
+        --disable-wall \
+        --disable-pylibmount \
+        --disable-bash-completion \
+        --without-python
+}
+
+compile_linuxutil() {
+    make ${MAKEOPTS}
+}
+
+install_linuxutil() {
+    make install
+}
+
+build_linuxutil() {
+    pushd "${WORKDIR}/util-linux-${LINUXUTILS_VERSION}"
+
+    prepare_linuxutil
+    compile_linuxutil
+    install_linuxutil
+
+    popd
+}
+
 #
 # Dynamic libraries management
 #
 ensure_libs() {
     echo "[+] verifing libraries dependancies"
     pushd "${ROOTDIR}"
+
+    if [ ! -e lib64 ]; then ln -s usr/lib lib64; fi
+    if [ ! -e lib ]; then ln -s lib64 lib; fi
+
+    cp -a /lib64/ld-* lib64/
 
     for file in $(find -type f -executable); do
         # Looking for dynamic libraries shared
@@ -365,7 +480,7 @@ ensure_libs() {
 
             # Grabbing library from host
             basename=$(echo $lib | awk -F'.so' '{ print $1 }')
-            cp -a $basename* lib/
+            cp -a $basename* usr/lib/
         done
     done
 
@@ -378,14 +493,26 @@ ensure_libs() {
 clean_root() {
     echo "[+] cleaning initramfs"
 
-    rm -rf "${ROOTDIR}"/include
-    rm -rf "${ROOTDIR}"/lib/*.a
-    rm -rf "${ROOTDIR}"/lib/*.la
-    rm -rf "${ROOTDIR}"/etc/init.d
-    rm -rf "${ROOTDIR}"/etc/udev
-    rm -rf "${ROOTDIR}"/usr/share/doc
-    rm -rf "${ROOTDIR}"/usr/share/man
-    rm -rf "${ROOTDIR}"/lib/pkgconfig
+    pushd "${ROOTDIR}"
+    mkdir -p dev mnt proc root sys tmp
+    rm -rf lib/*.a
+    rm -rf lib/*.la
+    rm -rf etc/init.d
+    rm -rf etc/udev
+    rm -rf usr/lib/*.a
+    rm -rf usr/lib/*.la
+    rm -rf usr/share/doc
+    rm -rf usr/share/man
+    rm -rf usr/share/locale
+    rm -rf usr/share/info
+    rm -rf usr/share/bash-completion
+    rm -rf usr/lib/pkgconfig
+    rm -rf usr/include
+    popd
+
+    # copy init
+    cp "${CONFDIR}/init" "${ROOTDIR}/init"
+    chmod +x "${ROOTDIR}/init"
 }
 
 main() {
@@ -411,12 +538,11 @@ main() {
         build_busybox
     fi
 
-    if [[ $DO_ALL == 1 ]] || [[ $DO_FUSE == 1 ]]; then
+    if [[ $DO_ALL == 1 ]] || [[ $DO_TOOLS == 1 ]]; then
         build_fuse
-    fi
-
-    if [[ $DO_ALL == 1 ]] || [[ $DO_CERTS == 1 ]]; then
         build_certs
+        build_parted
+        build_linuxutil
     fi
 
     if [[ $DO_ALL == 1 ]] || [[ $DO_CORES == 1 ]]; then
