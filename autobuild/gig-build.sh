651bd161e2c95d67422a07be10379101ae03fbe6
Maxime Daniel <root@maxux.net>
2017-04-11 17:47:16 +0200

    Adding autobuild script

---
+++ autobuild/gig-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install dependencies for building
+apt-get update
+apt-get install -y asciidoc xmlto --no-install-recommends
+apt-get install -y xz-utils pkg-config lbzip2 make curl libtool gettext m4 autoconf uuid-dev libncurses5-dev libreadline-dev bc e2fslibs-dev uuid-dev libattr1-dev zlib1g-dev libacl1-dev e2fslibs-dev libblkid-dev liblzo2-dev git libbison-dev flex libmnl-dev xtables-addons-source libglib2.0-dev libfuse-dev libxml2-dev libdevmapper-dev libpciaccess-dev libnl-3-dev libnl-route-3-dev libyajl-dev dnsmasq liblz4-dev libsnappy-dev libbz2-dev libssl-dev gperf libelf-dev libkmod-dev liblzma-dev git kmod libvirt-dev
+
+# install go
+curl https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz > /tmp/go1.8.linux-amd64.tar.gz
+tar -C /usr/local -xzf /tmp/go1.8.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+mkdir /gopath
+export GOPATH=/gopath
+
+# start the build
+cd /initramfs
+bash initramfs.sh
