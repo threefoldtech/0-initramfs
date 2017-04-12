c4ec8adaa66a25c088b1d035f4b594cd00a16cd8
Maxime Daniel <root@maxux.net>
2017-04-12 11:41:24 +0200

    Update core0 version during submodule call

--- autobuild/gig-build-submodules.sh
+++ autobuild/gig-build-submodules.sh
@@ -4,10 +4,18 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
-echo "Updating to version: $1"
-
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=/gopath
+
+echo "[+] updating to version: $1"
+cd $GOPATH/src/github.com/g8os/core0
+
+echo "[+] fetching code"
+git fetch origin "$1:$1"
+
+echo "[+] checkout repository"
+git checkout 1.1.0-alpha
+git pull origin 1.1.0-alpha
 
 # start the build
 cd /initramfs
