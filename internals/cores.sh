4a7229b44f912ffa27c53148f02be129f3c9faf6
Muhamad Azmy <muhamad.azmy@codescalers.com>
2017-07-04 09:19:29 +0200

    Building ztid and add it to the image (#50)
    * Building ztid and add it to the image
    
    * Convert tabs to spaces

--- internals/cores.sh
+++ internals/cores.sh
@@ -11,6 +11,9 @@ prepare_cores() {
     echo "[+] loading source code: g8ufs"
     go get -d -v github.com/zero-os/0-fs
 
+    echo "[+] loading soruce code: ztid"
+    go get -d -v github.com/zero-os/ztid
+
     echo "[+] ensure core0 to branch: ${CORES_VERSION}"
     pushd $GOPATH/src/github.com/zero-os/0-core
     branch=$(git rev-parse --abbrev-ref HEAD)
@@ -36,34 +39,51 @@ prepare_cores() {
 
 compile_cores() {
     echo "[+] compiling coreX and core0"
+    pushd 0-core
     make
+    popd
 
     echo "[+] compiling 0-fs"
-    pushd ../0-fs
+    pushd 0-fs
     make
+    popd
+
+    echo "[+] compiling ztid"
+    pushd ztid
+    go build
     popd
 }
 
 install_cores() {
     echo "[+] copying binaries"
+    pushd 0-core
     cp -a bin/* "${ROOTDIR}/sbin/"
     cp -a tools/* "${ROOTDIR}/usr/bin/"
-    cp -a ../0-fs/g8ufs "${ROOTDIR}/sbin/"
+
+    echo "	[+] installing configuration"
+    mkdir -p "${ROOTDIR}/etc/zero-os/conf"
+    cp -a core0/conf/* "${ROOTDIR}"/etc/zero-os/conf/
+    rm -f "${ROOTDIR}"/etc/zero-os/conf/README.md
+    popd
+
+    pushd 0-fs
+    cp -a g8ufs "${ROOTDIR}/sbin/"
+    popd
+
+    pushd ztid
+    cp -a ztid "${ROOTDIR}/sbin/"
+    popd
+
     pushd "${ROOTDIR}/sbin"
     ln -sf corectl reboot
     ln -sf corectl poweroff
     popd
-
-    echo "[+] installing configuration"
-    mkdir -p "${ROOTDIR}/etc/zero-os/conf"
-    cp -a core0/conf/* "${ROOTDIR}"/etc/zero-os/conf/
-    rm -f "${ROOTDIR}"/etc/zero-os/conf/README.md
 }
 
 build_cores() {
     # We need to prepare first (download code)
     prepare_cores
-    pushd $GOPATH/src/github.com/zero-os/0-core
+    pushd $GOPATH/src/github.com/zero-os
 
     compile_cores
     install_cores
