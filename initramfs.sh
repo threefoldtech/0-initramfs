7eff4fa226d9493740b58533394090af2d0c5645
Maxime Daniel <root@maxux.net>
2018-03-19 16:32:34 +0100

    Keep rocksdb needed by cores when rebuilding only them

--- initramfs.sh
+++ initramfs.sh
@@ -420,6 +420,7 @@ clean_staging() {
     rm -rf "${TMPDIR}"/*
     mv -f "${WORKDIR}"/linux-* "${TMPDIR}"/
     mv -f "${WORKDIR}"/vmlinuz* "${TMPDIR}"/
+    mv -f "${WORKDIR}"/rocksdb* "${TMPDIR}"/
 
     # cleaning staging files
     rm -rf "${WORKDIR}"/*
