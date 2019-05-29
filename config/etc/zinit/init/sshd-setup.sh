48e3c7ab699e35f5fc86b223437cf556aa51055d
Maxime Daniel <root@maxux.net>
2019-05-29 03:39:48 +0200

    Make ssh working on boot

--- config/etc/zinit/init/sshd-setup.sh
+++ config/etc/zinit/init/sshd-setup.sh
@@ -1,3 +1,14 @@
 #!/bin/ash
+if [ -f /etc/ssh/ssh_host_rsa_key ]; then
+    # nothing to do, hackish way
+    sleep 10
+    exit 0
+fi
+
 echo "Setting up sshd"
 mkdir -p /run/sshd
+
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
+ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
