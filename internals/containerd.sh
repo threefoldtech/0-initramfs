8491226b6043b064ebd54f2bd107c21539906416
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-06-24 12:16:18 +0200

    WIP: containerd builder

---
+++ internals/containerd.sh
@@ -0,0 +1,46 @@
+CONTAINERD_VERSION="v1.2.7"
+
+github_force() {
+    if [ -d $3 ]; then
+        pushd $3
+        git fetch
+        git checkout $2
+        git pull origin $2
+        popd
+
+    else
+        git clone https://github.com/$1 $3
+        pushd $3
+        git checkout $2
+        popd
+    fi
+}
+
+prepare_containerd() {
+    echo "[+] loading source code: containerd"
+    github_force containerd/containerd $CONTAINERD_VERSION containerd
+}
+
+compile_containerd() {
+    echo "[+] compiling coreX and core0"
+    pushd containerd
+    make
+    popd
+}
+
+install_containerd() {
+    echo "[+] copying binaries"
+    pushd containerd
+    cp -a bin/* "${ROOTDIR}/bin/"
+}
+
+build_containerd() {
+    mkdir -p $GOPATH/src/github.com/containerd
+    pushd $GOPATH/src/github.com/containerd
+
+    prepare_containerd
+    compile_containerd
+    install_containerd
+
+    popd
+}
