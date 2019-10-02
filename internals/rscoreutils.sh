ba88b545c7361166cfec061b3cef33e4065e5186
Muhamad Azmy <muhamada@greenitglobe.com>
2019-10-02 10:00:48 +0200

    Development zos v2 stdbuf (#25)
    * Build stdbuf from coreutils
    
    * fix indendation
    
    * fix path
    
    * Because i can't get it right from first time!
    
    * Apply review comments
    
    * Rename coreutils to rscoreutils
    
    * fix cp -v flag
    
    * Update build script to use go 1.13
    
    * Update autobuild to use go 1.13.1

---
+++ internals/rscoreutils.sh
@@ -0,0 +1,49 @@
+RSCOREUTILS_REPOSITORY="https://github.com/uutils/coreutils.git"
+RSCOREUTILS_VERSION="master"
+
+download_rscoreutils() {
+    download_git ${RSCOREUTILS_REPOSITORY} ${RSCOREUTILS_VERSION}
+}
+
+extract_rscoreutils() {
+    event "refreshing" "coreutils-${RSCOREUTILS_VERSION}"
+    rm -rf ./coreutils-${RSCOREUTILS_VERSION}
+    cp -a ${DISTFILES}/coreutils ./coreutils-${RSCOREUTILS_VERSION}
+}
+
+prepare_rscoreutils() {
+    echo "[+] loading source code: coreutils"
+}
+
+compile_rscoreutils() {
+    echo "[+] compiling coreutils"
+    # we only compile libstdbuf because that's all what
+    # we need from library.
+    pushd src/stdbuf/libstdbuf
+
+    cargo build --release
+
+    popd
+}
+
+install_rscoreutils() {
+    echo "[+] copying binaries"
+    cp -va target/release/liblibstdbuf.so "${ROOTDIR}/lib/libstdbuf.so"
+}
+
+build_rscoreutils() {
+    pushd "${WORKDIR}/coreutils-${RSCOREUTILS_VERSION}"
+
+    prepare_rscoreutils
+    compile_rscoreutils
+    install_rscoreutils
+
+    popd
+}
+
+registrar_rscoreutils() {
+    DOWNLOADERS+=(download_rscoreutils)
+    EXTRACTORS+=(extract_rscoreutils)
+}
+
+registrar_rscoreutils
