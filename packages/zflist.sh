2d13fba4247a1651f9c70a10bf192d05f8774c34
Maxime Daniel <root@maxux.net>
2020-09-29 19:12:39 +0200

    Freeze 0-flist version 2.0.0-rc1

--- packages/zflist.sh
+++ packages/zflist.sh
@@ -1,19 +1,21 @@
-ZFLIST_REPOSITORY="https://github.com/threefoldtech/0-flist"
-ZFLIST_BRANCH="development"
+ZFLIST_VERSION="2.0.0-rc1"
+ZFLIST_CHECKSUM="c4dc7b9165c2024eaa0bc24cd26cb26a"
+ZFLIST_LINK="https://github.com/threefoldtech/0-flist/archive/v${ZFLIST_VERSION}.tar.gz"
 
 CAPNPC_VERSION="0.3"
 CAPNPC_CHECKSUM="c1836601d210c14a4a88ed55e0b7c6de"
 CAPNPC_LINK="https://github.com/opensourcerouting/c-capnproto/releases/download/c-capnproto-${CAPNPC_VERSION}/c-capnproto-${CAPNPC_VERSION}.tar.xz"
 
 download_zflist() {
-    download_git $ZFLIST_REPOSITORY $ZFLIST_BRANCH
+    download_file $ZFLIST_LINK $ZFLIST_CHECKSUM 0-flist-${ZFLIST_VERSION}.tar.gz
     download_file $CAPNPC_LINK $CAPNPC_CHECKSUM
 }
 
 extract_zflist() {
-    event "refreshing" "zflist-${ZFLIST_BRANCH}"
-    rm -rf ./zflist-${ZFLIST_BRANCH}
-    cp -a ${DISTFILES}/0-flist ./zflist-${ZFLIST_BRANCH}
+    if [ ! -d "0-flist-${ZFLIST_VERSION}" ]; then
+        echo "[+] extracting: 0-flist-${ZFLIST_VERSION}"
+        tar -xf ${DISTFILES}/0-flist-${ZFLIST_VERSION}.tar.gz -C .
+    fi
 
     if [ ! -d "c-capnproto-${CAPNPC_VERSION}" ]; then
         echo "[+] extracting: c-capnproto-${CAPNPC_VERSION}"
@@ -56,7 +58,7 @@ build_zflist() {
 
     popd
 
-    pushd "${WORKDIR}/zflist-${ZFLIST_BRANCH}"
+    pushd "${WORKDIR}/0-flist-${ZFLIST_VERSION}"
 
     prepare_zflist
     compile_zflist
