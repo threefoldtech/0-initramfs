db0e42fca66fa8d4eeb6de190266020cd42a0420
Maxime Daniel <root@maxux.net>
2021-01-16 03:40:42 +0100

    snappy: add package with cross-compilation

---
+++ packages/snappy.sh
@@ -0,0 +1,52 @@
+SNAPPY_PKGNAME="snappy"
+SNAPPY_VERSION="1.1.8"
+SNAPPY_CHECKSUM="70e48cba7fecf289153d009791c9977f"
+SNAPPY_LINK="https://github.com/google/snappy/archive/${SNAPPY_VERSION}.tar.gz"
+
+download_snappy() {
+    download_file $SNAPPY_LINK $SNAPPY_CHECKSUM snappy-${SNAPPY_VERSION}.tar.gz
+}
+
+extract_snappy() {
+    if [ ! -d "${SNAPPY_PKGNAME}-${SNAPPY_VERSION}" ]; then
+        echo "[+] extracting: ${SNAPPY_PKGNAME}-${SNAPPY_VERSION}"
+        tar -xf ${DISTFILES}/${SNAPPY_PKGNAME}-${SNAPPY_VERSION}.tar.gz -C .
+    fi
+}
+
+prepare_snappy() {
+    echo "[+] configuring: ${SNAPPY_PKGNAME}"
+
+    cmake -DCMAKE_INSTALL_PREFIX=/usr \
+        -DCMAKE_C_COMPILER=${BUILDHOST}-gcc \
+        -DCMAKE_CXX_COMPILER=${BUILDHOST}-g++ \
+        -DHAVE_LIBZ=NO \
+        -DHAVE_LIBLZO2=NO
+}
+
+compile_snappy() {
+    echo "[+] compiling: ${SNAPPY_PKGNAME}"
+    make ${MAKEOPTS}
+}
+
+install_snappy() {
+    echo "[+] installing: ${SNAPPY_PKGNAME}"
+    make DESTDIR="${ROOTDIR}" install
+}
+
+build_snappy() {
+    pushd "${WORKDIR}/${SNAPPY_PKGNAME}-${SNAPPY_VERSION}"
+
+    prepare_snappy
+    compile_snappy
+    install_snappy
+
+    popd
+}
+
+registrar_snappy() {
+    DOWNLOADERS+=(download_snappy)
+    EXTRACTORS+=(extract_snappy)
+}
+
+registrar_snappy
