abbc32b72948549aa6449693133b3589f0e32a61
Maxime Daniel <root@maxux.net>
2021-01-16 23:19:48 +0100

    tcpdump: split libpcap and install libpcap in the system

---
+++ packages/libpcap.sh
@@ -0,0 +1,59 @@
+LIBPCAP_PKGNAME="libpcap"
+LIBPCAP_VERSION="1.9.0"
+LIBPCAP_CHECKSUM="dffd65cb14406ab9841f421732eb0f33"
+LIBPCAP_LINK="https://www.tcpdump.org/release/libpcap-${LIBPCAP_VERSION}.tar.gz"
+
+download_libpcap() {
+    download_file $LIBPCAP_LINK $LIBPCAP_CHECKSUM
+}
+
+extract_libpcap() {
+    if [ ! -d "${LIBPCAP_PKGNAME}-${LIBPCAP_VERSION}" ]; then
+        progress "extracting: ${LIBPCAP_PKGNAME}-${LIBPCAP_VERSION}"
+        tar -xf ${DISTFILES}/${LIBPCAP_PKGNAME}-${LIBPCAP_VERSION}.tar.gz -C .
+    fi
+}
+
+prepare_libpcap() {
+    progress "preparing: ${LIBPCAP_PKGNAME}"
+
+    ./configure --prefix=/usr \
+        --build=${BUILDCOMPILE} \
+        --host=${BUILDHOST} \
+        --enable-ipv6 \
+        --disable-remote \
+        --disable-usb \
+        --disable-netmap \
+        --disable-bluetooth \
+        --disable-dbus \
+        --disable-rdma
+}
+
+compile_libpcap() {
+    progress "compiling: ${LIBPCAP_PKGNAME}"
+
+    make ${MAKEOPTS}
+}
+
+install_libpcap() {
+    progress "installing: ${LIBPCAP_PKGNAME}"
+
+    make DESTDIR=${ROOTDIR} install
+}
+
+build_libpcap() {
+    pushd "${WORKDIR}/${LIBPCAP_PKGNAME}-${LIBPCAP_VERSION}"
+
+    prepare_libpcap
+    compile_libpcap
+    install_libpcap
+
+    popd
+}
+
+registrar_libpcap() {
+    DOWNLOADERS+=(download_libpcap)
+    EXTRACTORS+=(extract_libpcap)
+}
+
+registrar_libpcap
--- packages/tcpdump.sh
+++ packages/tcpdump.sh
@@ -1,67 +1,39 @@
+TCPDUMP_PKGNAME="tcpdump"
 TCPDUMP_VERSION="4.9.2"
 TCPDUMP_CHECKSUM="9bbc1ee33dab61302411b02dd0515576"
 TCPDUMP_LINK="https://www.tcpdump.org/release/tcpdump-${TCPDUMP_VERSION}.tar.gz"
 
-LIBPCAP_VERSION="1.9.0"
-LIBPCAP_CHECKSUM="dffd65cb14406ab9841f421732eb0f33"
-LIBPCAP_LINK="https://www.tcpdump.org/release/libpcap-${LIBPCAP_VERSION}.tar.gz"
-
-
 download_tcpdump() {
     download_file $TCPDUMP_LINK $TCPDUMP_CHECKSUM
-    download_file $LIBPCAP_LINK $LIBPCAP_CHECKSUM
 }
 
 extract_tcpdump() {
-    if [ ! -d "tcpdump-${TCPDUMP_VERSION}" ]; then
-        echo "[+] extracting: tcpdump-${TCPDUMP_VERSION}"
-        tar -xf ${DISTFILES}/tcpdump-${TCPDUMP_VERSION}.tar.gz -C .
+    if [ ! -d "${TCPDUMP_PKGNAME}-${TCPDUMP_VERSION}" ]; then
+        progress "extracting: ${TCPDUMP_PKGNAME}-${TCPDUMP_VERSION}"
+        tar -xf ${DISTFILES}/${TCPDUMP_PKGNAME}-${TCPDUMP_VERSION}.tar.gz -C .
     fi
-
-    if [ ! -d "libpcap-${LIBPCAP_VERSION}" ]; then
-        echo "[+] extracting: libpcap-${LIBPCAP_VERSION}"
-        tar -xf ${DISTFILES}/libpcap-${LIBPCAP_VERSION}.tar.gz -C .
-    fi
-
-}
-
-build_libpcap() {
-    echo "[+] preparing libcap"
-
-    ./configure --prefix=/usr \
-        --build=${BUILDCOMPILE} \
-        --host=${BUILDHOST} \
-        --enable-ipv6 \
-        --disable-remote \
-        --disable-usb \
-        --disable-netmap \
-        --disable-bluetooth \
-        --disable-dbus \
-        --disable-rdma
-
-    make ${MAKEOPTS}
-
-    # no need to install, tcpdump will automatically find
-    # this libpcap on parent directory and static link against it
-    # make DESTDIR=${ROOTDIR} install-shared
 }
 
 prepare_tcpdump() {
-    echo "[+] preparing tcpdump"
+    progress "preparing: ${TCPDUMP_PKGNAME}"
+
     ./configure --prefix=/usr \
         --build=${BUILDCOMPILE} \
         --host=${BUILDHOST} \
         --disable-smb \
+        --with-system-libpcap \
         --without-smi
 }
 
 compile_tcpdump() {
-    echo "[+] compiling tcpdump"
+    progress "compiling: ${TCPDUMP_PKGNAME}"
+
     make ${MAKEOPTS}
 }
 
 install_tcpdump() {
-    echo "[+] installing tcpdump"
+    progress "installing: ${TCPDUMP_PKGNAME}"
+
     make DESTDIR=${ROOTDIR} install
 
     # remove duplicated binary
@@ -69,13 +41,7 @@ install_tcpdump() {
 }
 
 build_tcpdump() {
-    pushd "${WORKDIR}/libpcap-${LIBPCAP_VERSION}"
-
-    build_libpcap
-
-    popd
-
-    pushd "${WORKDIR}/tcpdump-${TCPDUMP_VERSION}"
+    pushd "${WORKDIR}/${TCPDUMP_PKGNAME}-${TCPDUMP_VERSION}"
 
     prepare_tcpdump
     compile_tcpdump
