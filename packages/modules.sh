db8820c82a618ae56d64653cd0134f8a32a737c1
Maxime Daniel <root@maxux.net>
2021-09-29 15:25:57 +0200

    zos: use main branch for v3

--- packages/modules.sh
+++ packages/modules.sh
@@ -1,16 +1,22 @@
-MODULES_VERSION="0.4.3"
-MODULES_CHECKSUM="99fd8573891897543db73673b6f2016d"
-MODULES_LINK="https://github.com/threefoldtech/zos/archive/v${MODULES_VERSION}.tar.gz"
+# MODULES_VERSION="0.4.3"
+# MODULES_CHECKSUM="99fd8573891897543db73673b6f2016d"
+# MODULES_LINK="https://github.com/threefoldtech/zos/archive/v${MODULES_VERSION}.tar.gz"
+MODULES_REPOSITORY="https://github.com/threefoldtech/zos"
+MODULES_VERSION="main"
 
 download_modules() {
-    download_file $MODULES_LINK $MODULES_CHECKSUM zos-${MODULES_VERSION}.tar.gz
+    # download_file $MODULES_LINK $MODULES_CHECKSUM zos-${MODULES_VERSION}.tar.gz
+    download_git ${MODULES_REPOSITORY} ${MODULES_VERSION}
 }
 
 extract_modules() {
-    if [ ! -d "zos-${MODULES_VERSION}" ]; then
-        echo "[+] extracting: zos-${MODULES_VERSION}"
-        tar -xf ${DISTFILES}/zos-${MODULES_VERSION}.tar.gz -C .
-    fi
+    # if [ ! -d "zos-${MODULES_VERSION}" ]; then
+    #     echo "[+] extracting: zos-${MODULES_VERSION}"
+    #     tar -xf ${DISTFILES}/zos-${MODULES_VERSION}.tar.gz -C .
+    # fi
+    event "refreshing" "zos-${MODULES_VERSION}"
+    rm -rf ./zos-${MODULES_VERSION}
+    cp -a ${DISTFILES}/zos ./zos-${MODULES_VERSION}
 }
 
 prepare_modules() {
