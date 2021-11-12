09496e36d231f02f206c852c37f7dc9d40fa7af1
Muhamad Azamy <muhamad@incubaid.com>
2021-11-12 15:21:40 +0100

    Use mirror

--- packages/zinit.sh
+++ packages/zinit.sh
@@ -3,13 +3,14 @@ ZINIT_HASH="c7aabd96bc390d2f36795a53f8561ba9"
 ZINIT_BINARY="https://github.com/threefoldtech/zinit/releases/download/v${ZINIT_VERSION}/zinit"
 
 download_zinit() {
-    download_file ${ZINIT_BINARY} ${ZINIT_HASH}
+    download_file ${ZINIT_BINARY} ${ZINIT_HASH} "zinit-${ZINIT_VERSION}"
 }
 
 install_zinit() {
     echo "[+] copying binaries"
-    chmod +x ${DISTFILES}/zinit
-    cp -a ${DISTFILES}/zinit "${ROOTDIR}/sbin/"
+    filepath="${DISTFILES}/zinit-${ZINIT_VERSION}"
+    chmod +x ${filepath}
+    cp -a "${filepath}" "${ROOTDIR}/sbin/zinit"
 }
 
 build_zinit() {
