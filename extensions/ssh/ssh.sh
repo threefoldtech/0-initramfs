99f59b12b7a3e99c44b1598135b5b800c63963b6
Muhamad Azamy <muhamad@incubaid.com>
2021-11-13 09:12:00 +0100

    fix extension

--- extensions/ssh/ssh.sh
+++ extensions/ssh/ssh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir -m 600 -p "${ROOTDIR}"/root/.ssh
+echo "ROOT: ${ROOTDIR}"
+mkdir -m 700 -p "${ROOTDIR}"/root/.ssh
 rm -f "${ROOTDIR}"/root/.ssh/authorized_keys
 
 if [ "${BUILDMODE}" = "debug" ]; then
-    for user in "muhamadazmy delandtj maxux LeeSmet DylanVerstraete"; do
+    for user in muhamadazmy delandtj maxux LeeSmet DylanVerstraete; do
         echo "[+] authorizing ssh key: $user"
         wget https://github.com/${user}.keys -O- >> "${ROOTDIR}"/root/.ssh/authorized_keys
     done
