5226c48552b25474e5d51bdd93bacf723c0c6bb5
Muhamad Azamy <muhamad@incubaid.com>
2021-12-14 22:36:19 +0100

    Update to latest zinit version

--- packages/zinit.sh
+++ packages/zinit.sh
@@ -1,5 +1,5 @@
-ZINIT_VERSION="0.2.1"
-ZINIT_HASH="c7aabd96bc390d2f36795a53f8561ba9"
+ZINIT_VERSION="0.2.5"
+ZINIT_HASH="9c7dc0fa99111b7b33f89bb298ce94c0"
 ZINIT_BINARY="https://github.com/threefoldtech/zinit/releases/download/v${ZINIT_VERSION}/zinit"
 
 download_zinit() {
