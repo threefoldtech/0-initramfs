a5a4c2b462cdebd8f2ae10842d21e7340eb8f2d5
Muhamad Azamy <muhamad@incubaid.com>
2023-04-06 13:04:46 +0200

    Add brandon to list of keys
    Also this mainly to force rebuild

--- extensions/ssh/ssh.sh
+++ extensions/ssh/ssh.sh
@@ -5,7 +5,7 @@ mkdir -m 700 -p "${ROOTDIR}"/root/.ssh
 rm -f "${ROOTDIR}"/root/.ssh/authorized_keys
 
 if [ "${BUILDMODE}" = "debug" ]; then
-    for user in muhamadazmy delandtj maxux LeeSmet DylanVerstraete coesensbert; do
+    for user in muhamadazmy delandtj maxux LeeSmet DylanVerstraete coesensbert brandonpille; do
         echo "[+] authorizing ssh key: $user"
         wget https://github.com/${user}.keys -O- >> "${ROOTDIR}"/root/.ssh/authorized_keys
     done
