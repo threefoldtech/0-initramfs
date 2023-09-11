cfbabbfb0ba5c83429b422f447ecd44b695299f9
Maxime Daniel <root@maxux.net>
2023-09-11 10:38:21 +0200

    kernel: bump linux-firmware 20230804

--- packages/linux-firmware.sh
+++ packages/linux-firmware.sh
@@ -1,5 +1,5 @@
-FIRMWARE_VERSION="20230515"
-FIRMWARE_CHECKSUM="fa8477de02a7c16ebf0ed599c6a03367"
+FIRMWARE_VERSION="20230804"
+FIRMWARE_CHECKSUM="bc60c0972db75e4cd46bf0dca7bd9347"
 FIRMWARE_LINK="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot/linux-firmware-${FIRMWARE_VERSION}.tar.gz"
 
 download_firmware() {
