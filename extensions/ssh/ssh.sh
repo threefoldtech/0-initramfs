9a06f04f4e327faf82e7a8134a40ee10bb037964
Maxime Daniel <root@maxux.net>
2024-09-07 00:43:40 +0200

    extensions: ssh: insert only the last key of each user

--- extensions/ssh/ssh.sh
+++ extensions/ssh/ssh.sh
@@ -6,7 +6,9 @@ rm -f "${ROOTDIR}/root/.ssh/authorized_keys"
 if [ "${BUILDMODE}" == "debug" ]; then
     for user in muhamadazmy delandtj maxux LeeSmet coesensbert; do
         echo "[+] authorizing ssh key: ${user}"
-        wget https://github.com/${user}.keys -O- >> "${ROOTDIR}/root/.ssh/authorized_keys"
+
+        key=$(curl -s https://github.com/${user}.keys | tail -1)
+        echo "${key} ${user}@initramfs" >> "${ROOTDIR}/root/.ssh/authorized_keys"
     done
 
     chmod -f 600 "${ROOTDIR}/root/.ssh/authorized_keys"
