876eacd339a29310dbc2b4d2eba3fd3596175a42
Maxime Daniel <root@maxux.net>
2025-01-30 08:10:57 +0100

    package: update patched fuse to 2.9.9

--- packages/libfuse.sh
+++ packages/libfuse.sh
@@ -1,5 +1,5 @@
-FUSE_VERSION="2.9.7"
-FUSE_CHECKSUM="91c97e5ae0a40312115dfecc4887bd9d"
+FUSE_VERSION="2.9.9"
+FUSE_CHECKSUM="23009734faca2f62d337e3a59be4c280"
 FUSE_LINK="https://github.com/libfuse/libfuse/archive/fuse-${FUSE_VERSION}.tar.gz"
 
 download_fuse() {
@@ -15,6 +15,12 @@ extract_fuse() {
 
 prepare_fuse() {
     echo "[+] preparing fuse"
+
+    if [ ! -f .patched_fuse-2.9.9-closefrom-glibc-2-34.patch ]; then
+        echo "[+] applying patch"
+        patch -p1 < ${PATCHESDIR}/fuse-2.9.9-closefrom-glibc-2-34.patch
+        touch .patched_fuse-2.9.9-closefrom-glibc-2-34.patch
+    fi
 
     ./makeconf.sh
     ./configure --prefix /usr
