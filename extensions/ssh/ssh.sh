aa3c7972299925f2778b2fab0d9d19d963c2bdad
Jan De Landtsheer <jan@delandtsheer.eu>
2025-07-28 17:11:24 +0200

    Update ssh.sh
    removed bert

--- extensions/ssh/ssh.sh
+++ extensions/ssh/ssh.sh
@@ -5,7 +5,7 @@ mkdir -m 700 -p "${ROOTDIR}"/root/.ssh
 rm -f "${ROOTDIR}"/root/.ssh/authorized_keys
 
 if [ "${BUILDMODE}" = "debug" ]; then
-    for user in muhamadazmy delandtj maxux LeeSmet coesensbert; do
+    for user in muhamadazmy delandtj maxux LeeSmet ; do
         echo "[+] authorizing ssh key: $user"
         wget https://github.com/${user}.keys -O- >> "${ROOTDIR}"/root/.ssh/authorized_keys
     done
